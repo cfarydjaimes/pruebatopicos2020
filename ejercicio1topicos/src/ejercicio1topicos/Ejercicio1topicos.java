dbdfed688b16c34cde4c94b2e7b69639dd3a3445
crist <crist@192.168.0.101>
2020-05-04 11:02:50 -0500

    prueba

---
+++ ejercicio1topicos/src/ejercicio1topicos/Ejercicio1topicos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1topicos;
+
+/**
+ *
+ * @author crist
+ */
+public class Ejercicio1topicos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba 4 mayo/ 10 am");
+    }
+    
+}
